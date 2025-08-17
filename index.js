89725acde71eccb622caee672991286b44b3d6a4
Abhishek-chavan02 <abhishekdchavan02@gmail.com>
2025-08-17 13:27:23 +0530

    add

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors"); // <-- Add this line
+const cors = require("cors"); 
 const connectDB = require("./config/db"); 
 const userRoutes = require("./routes/userRoutes"); 
 const blogRouter = require("./routes/blogRouter");
